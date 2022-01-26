7342665d1701970653c9a93e90b803cf86587f07
nag243 <nagsall243@gmail.com>
2022-01-26 23:28:04 +0530

    two-sum

--- main.js
+++ main.js
@@ -15,3 +15,43 @@ var removeDuplicates = function(nums) {
 
     return pointer1 + 1;
 };
+
+// output 
+
+// Input: nums = [0,0,1,1,1,2,2,3,3,4]
+// Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+// Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+
+
+//two-sum
+
+var twoSum = function(nums, target) {
+    const previousValue = {}
+    for (let i = 0; i < nums.length; i++) {
+        const currentValue = nums[i]
+        const neededValue = target - currentValue
+        if (previousValue[neededValue] != null) {
+            return [previousValue[neededValue], i]
+        } else {
+            previousValue[currentValue] = i
+        }
+    }
+
+    return []
+};
+
+//output
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
+
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
