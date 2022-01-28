933b134a6e769ef5538548f24f9a17d6f8f3759e
nag243 <nagsall243@gmail.com>
2022-01-28 23:46:08 +0530

    Roman to Integer

--- main.js
+++ main.js
@@ -99,3 +99,32 @@ let isValidCharacter = function(char) {
     let validChars = 'abcdefghigklmnopqrstuvwxyz012345689';
     return validChars.indexOf(char.toLowerCase()) > -1
 }
+
+// Roman to Integer 
+
+var romanToInt = function(s) {
+    const map = { I: 1, V: 5, x: 10, L: 50, C: 100, D: 500, M: 1000 };
+    let res = 0;
+    s.split('').forEach((num, i) => {
+        if (map[num] < map[s[i + 1]]) res -= map[num];
+        else res += map[num]
+    });
+
+    return res
+};
+
+// Example 1:
+
+// Input: s = "III"
+// Output: 3
+// Explanation: III = 3.
+// Example 2:
+
+// Input: s = "LVIII"
+// Output: 58
+// Explanation: L = 50, V= 5, III = 3.
+// Example 3:
+
+// Input: s = "MCMXCIV"
+// Output: 1994
+// Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
