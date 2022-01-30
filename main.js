4285c4351d3cfc8faa3364df2a7e663925a2684f
nag243 <nagsall243@gmail.com>
2022-01-30 23:27:36 +0530

    Search Insert Position

--- main.js
+++ main.js
@@ -143,3 +143,28 @@ var longestCommonPrefix = function(strs) {
     }
     return prefix
 }
+
+// Search Insert Position
+var searchInsert = function(nums, target) {
+        let left = 0,
+            right = nums.length - 1;
+        if (target < nums[left]) return 0;
+        if (target > nums[right]) return nums.length;
+        while (left <= right) {
+            let mid = (left + right) >> 1;
+            if (target === nums[mid]) return mid;
+            else if (target < nums[mid]) right = mid - 1;
+            else left = mid + 1;
+        }
+        return left
+    }
+    //  Input: nums = [1,3,5,6], target = 5
+    // Output: 2
+    // Example 2:
+
+// Input: nums = [1,3,5,6], target = 2
+// Output: 1
+// Example 3:
+
+// Input: nums = [1,3,5,6], target = 7
+// Output: 4
