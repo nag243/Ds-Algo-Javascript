1a3a78f397e662421b3201bfd802c0867438797e
nag243 <nagsall243@gmail.com>
2022-02-04 22:43:02 +0530

    Single Number

--- main.js
+++ main.js
@@ -253,3 +253,34 @@ var isValid = function(s) {
 
 // Input: s = "(]"
 // Output: false
+
+
+
+// Single Number
+
+var singleNumber = function(nums) {
+    const ht = {};
+
+    for (const num of nums) {
+        ht[num] = ht[num] + 1 || 1;
+    }
+
+    for (const key in ht) {
+        if (ht[key] == 1) {
+            return key
+        }
+    }
+}
+
+// Example 1:
+
+// Input: nums = [2,2,1]
+// Output: 1
+// Example 2:
+
+// Input: nums = [4,1,2,1,2]
+// Output: 4
+// Example 3:
+
+// Input: nums = [1]
+// Output: 1
