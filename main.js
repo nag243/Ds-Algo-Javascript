e599f48ee9d09ab25f11a27df9f93443f6874a56
nag243 <nagsall243@gmail.com>
2022-02-05 21:29:33 +0530

    Plus one

--- main.js
+++ main.js
@@ -284,3 +284,33 @@ var singleNumber = function(nums) {
 
 // Input: nums = [1]
 // Output: 1
+
+// Plus one
+var plusOne = function(digits) {
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i] = digits[i] + 1
+            return digits
+        } else {
+            digits[i] = 0;
+        }
+    }
+
+    digits.unshift(1);
+    return digits;
+}
+
+// Example 1:
+
+// Input: digits = [1,2,3]
+// Output: [1,2,4]
+// Explanation: The array represents the integer 123.
+// Incrementing by one gives 123 + 1 = 124.
+// Thus, the result should be [1,2,4].
+// Example 2:
+
+// Input: digits = [4,3,2,1]
+// Output: [4,3,2,2]
+// Explanation: The array represents the integer 4321.
+// Incrementing by one gives 4321 + 1 = 4322.
+// Thus, the result should be [4,3,2,2].
