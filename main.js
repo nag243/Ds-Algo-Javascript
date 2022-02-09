90a018236a96dda69671d59e9b775e056a06699b
nag243 <nagsall243@gmail.com>
2022-02-09 23:29:06 +0530

    Intersection of Two Arrays

--- main.js
+++ main.js
@@ -407,3 +407,34 @@ var isHappy = function(n) {
 
 // Input: n = 2
 // Output: false
+
+
+// Intersection of Two Arrays
+
+var intersection = function(num1, num2) {
+    let firstSet = new Set();
+
+    for (let num of num1) {
+        firstSet.add(num);
+    };
+
+    let intersectionSet = new Set()
+
+    for (let num of num2) {
+        if (firstSet.has(num)) {
+            intersectionSet.add(num)
+        }
+    }
+
+    return Array.from(intersectionSet)
+}
+
+// Example 1:
+
+// Input: nums1 = [1,2,2,1], nums2 = [2,2]
+// Output: [2]
+// Example 2:
+
+// Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+// Output: [9,4]
+// Explanation: [4,9] is also accepted.
