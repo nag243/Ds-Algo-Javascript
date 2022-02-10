3ded32fdaaa3324b9a537b27103b179880f77132
nag243 <nagsall243@gmail.com>
2022-02-10 23:29:53 +0530

    Best Time to Buy and Sell Stock

--- main.js
+++ main.js
@@ -438,3 +438,28 @@ var intersection = function(num1, num2) {
 // Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
 // Output: [9,4]
 // Explanation: [4,9] is also accepted.
+
+
+//  Best Time to Buy and Sell Stock
+
+var maxProfit = function(prices) {
+    let maxProf = 0;
+    let minVal = Infinity;
+    for (let i = 0; i < prices.length; i++) {
+        minVal = Math.min(minVal, prices[i]);
+        maxProf = Math.max(maxProf, prices[i] - minVal);
+    }
+    return maxProf;
+}
+
+// Example 1:
+
+// Input: prices = [7,1,5,3,6,4]
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+// Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+// Example 2:
+
+// Input: prices = [7,6,4,3,1]
+// Output: 0
+// Explanation: In this case, no transactions are done and the max profit = 0.
