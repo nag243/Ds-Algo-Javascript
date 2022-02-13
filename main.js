d40593bd484c3c777f82e4ef17bb3de68d6945d7
nag243 <nagsall243@gmail.com>
2022-02-13 23:16:02 +0530

    Isomorphic Strings

--- main.js
+++ main.js
@@ -489,3 +489,38 @@ var majorityElement = function(nums) {
     }
     return max
 };
+
+// 205 -  Isomorphic Strings
+
+var isIsomorphic = function(s, t) {
+    if (s.length != t.length) {
+        return false
+    }
+    let sArr = new Array(128).fill(0);
+    let tArr = new Array(128).fill(0)
+
+    for (let i = 0; i < s.length; i++) {
+        let sIdx = s.charCodeAt(i);
+        let tIdx = t.charCodeAt(i);
+        if (sArr[sIdx] != tArr[tIdx]) {
+            return false
+        }
+
+        sArr[sIdx] = i + 1
+        tArr[tIdx] = i + 1
+    }
+    return true
+}
+
+// Example 1:
+
+// Input: s = "egg", t = "add"
+// Output: true
+// Example 2:
+
+// Input: s = "foo", t = "bar"
+// Output: false
+// Example 3:
+
+// Input: s = "paper", t = "title"
+// Output: true
