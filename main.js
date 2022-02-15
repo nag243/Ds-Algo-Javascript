879936f9c9bc5ea49093a7fc825d6719433dc8bf
nag243 <nagsall243@gmail.com>
2022-02-15 22:54:12 +0530

    21 Merge Two Sorted Lists

--- main.js
+++ main.js
@@ -524,3 +524,27 @@ var isIsomorphic = function(s, t) {
 
 // Input: s = "paper", t = "title"
 // Output: true
+
+// 21 Merge Two Sorted Lists
+
+var mergeTwoLists = function(l1, l2) {
+    let curr = new ListNode();
+    const dummy = curr;
+    while (l1 & l2) {
+        if (l1.val < l2.val) {
+            curr.next = l1
+            l1 = l1.next;
+        } else {
+            curr.next = l2
+            l2 = l2.next;
+        }
+        curr = curr.next
+    }
+    if (l1) {
+        curr.next = l1
+    }
+    if (l2) {
+        curr.next = l2
+    }
+    return dummy.next;
+}
