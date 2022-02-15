24877454f0e84ce68544db22afbb6e70948615dc
nag243 <nagsall243@gmail.com>
2022-02-15 23:16:08 +0530

    151. Reverse Words in a String

--- main.js
+++ main.js
@@ -564,3 +564,22 @@ function dfs(node, min, max) {
     }
     return dfs(node.left, min, node.val) && dfs(node.right, node.val, max)
 }
+
+// 151. Reverse Words in a String
+
+const reverseWords = s => s.trim().split(/\s+/).reverse().join(' ');
+
+// Example 1:
+
+// Input: s = "the sky is blue"
+// Output: "blue is sky the"
+// Example 2:
+
+// Input: s = "  hello world  "
+// Output: "world hello"
+// Explanation: Your reversed string should not contain leading or trailing spaces.
+// Example 3:
+
+// Input: s = "a good   example"
+// Output: "example good a"
+// Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
