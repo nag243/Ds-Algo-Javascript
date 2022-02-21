a4ed6ffbda128793bbc70885402476bccecca0da
nag243 <nagsall243@gmail.com>
2022-02-22 00:02:52 +0530

    permutations

--- main.js
+++ main.js
@@ -674,3 +674,41 @@ const reverseList = (head) => {
     }
     return previous
 }
+
+// permutations
+
+var permute = function(nums) {
+    //global result
+    const result = [];
+    //dfs recursive helper 
+    const dfs = (i, nums) => {
+        //base case
+        if (i === nums.length) {
+            result.push(nums.slice())
+            return
+        }
+
+        //  dfs recursive case 
+        for (let j = i; j < nums.length; j++) {
+            [nums[i], nums[j]] = [nums[j], nums[i]];
+            dfs(i + 1, nums);
+            [nums[i], nums[j]] = [nums[j], nums[i]]
+        }
+    }
+    dfs(0, nums);
+    return result;
+}
+
+
+// Example 1:
+
+// Input: nums = [1,2,3]
+// Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+// Example 2:
+
+// Input: nums = [0,1]
+// Output: [[0,1],[1,0]]
+// Example 3:
+
+// Input: nums = [1]
+// Output: [[1]]
