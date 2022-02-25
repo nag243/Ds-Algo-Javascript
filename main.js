7ed42307a2fea16e5ed8f5b4952ee2965f311cd0
nag243 <nagsall243@gmail.com>
2022-02-25 23:14:24 +0530

    3sum

--- main.js
+++ main.js
@@ -733,3 +733,31 @@ var groupAnagrams = function(strs) {
 
     return Array.from(result.values())
 };
+
+// /3sum
+
+var threeSum = function(nums) {
+    nums.sort((a, b) => a - b);
+    const results = [];
+    for (let i = 0; i < nums.length; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+        const target = 0 - nums[i];
+        let left = i + 1;
+        let right = nums.length - 1;
+        while (right > left) {
+            const sum = nums[left] + nums[right];
+            if (sum > target) {
+                right--;
+            } else if (sum < target) {
+                left++;
+            } else {
+                results.push([nums[i], nums[left], nums[right]]);
+                while (nums[left] === nums[left + 1]) left++;
+                while (nums[right] === nums[left - 1]) right--;
+                left++;
+                right--;
+            }
+        }
+    }
+    return results;
+}
