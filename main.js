4593b9232378662259cd7c1397c4d498742d2e81
nag243 <nagsall243@gmail.com>
2022-02-28 23:18:53 +0530

    Find Peak Element

--- main.js
+++ main.js
@@ -778,3 +778,32 @@ var findKthLargest = function(nums, k) {
     nums.sort((a, b) => { return a - b })
     return nums[nums.length - k]
 };
+
+//  Find Peak Element 
+
+var findPeakElement = function(nums) {
+    let left = 0;
+    let right = nums.length - 1;
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2);
+        if (nums[mid] > nums[mid + 1]) {
+            right = mid;
+        } else {
+            left = mid + 1
+        }
+    }
+    return left
+}
+
+/*
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: 2
+Explanation: 3 is a peak element and your function should return the index number 2.
+Example 2:
+
+Input: nums = [1,2,1,3,5,6,4]
+Output: 5
+Explanation: Your function can return either index number 1 where the peak element is 2, or index number 5 where the peak element is 6.
+*/
