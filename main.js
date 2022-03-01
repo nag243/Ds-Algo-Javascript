c0f0e462ed46e7043d1e1df27afaded59fa3fba0
nag243 <nagsall243@gmail.com>
2022-03-01 23:15:26 +0530

    56. Merge Intervals

--- main.js
+++ main.js
@@ -807,3 +807,34 @@ Input: nums = [1,2,1,3,5,6,4]
 Output: 5
 Explanation: Your function can return either index number 1 where the peak element is 2, or index number 5 where the peak element is 6.
 */
+
+// 56. Merge Intervals
+
+var merge = function(intervals) {
+        if (intervals.length === 1) return intervals;
+        intervals.sort((a, b) => a[0] - b[0]);
+        let curr = intervals[0];
+        let result = [curr];
+        for (let i = 1; i < intervals.length; i += 1) {
+            const currRight = curr[1];
+            const [nextLeft, nextRight] = intervals[i];
+            if (currRight >= nextLeft) {
+                curr[1] = Math.max(currRight, nextRight);
+            } else {
+                result.push(intervals[i]);
+                curr = intervals[i]
+            }
+        }
+        return result
+    }
+    /*
+    Example 1:
+    Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+    Output: [[1,6],[8,10],[15,18]]
+    Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+    Example 2:
+
+    Input: intervals = [[1,4],[4,5]]
+    Output: [[1,5]]
+    Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+    */
